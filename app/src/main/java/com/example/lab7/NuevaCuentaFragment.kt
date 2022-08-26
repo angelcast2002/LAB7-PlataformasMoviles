5af2e31b1708903a1e5b80d1876c24adf2d9fdb5
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-08-25 19:07:27 -0600

    V1.1
    Avances generales

---
+++ app/src/main/java/com/example/lab7/NuevaCuentaFragment.kt
@@ -0,0 +1,5 @@
+package com.example.lab7
+
+import androidx.fragment.app.Fragment
+class NuevaCuentaFragment: Fragment(R.layout.nueva_cuenta_fragment){
+}
---
+++ app/src/main/java/com/example/lab7/PerfilFragment.kt
@@ -0,0 +1,5 @@
+package com.example.lab7
+
+import androidx.fragment.app.Fragment
+class PerfilFragment: Fragment(R.layout.perfil_fragment){
+}
